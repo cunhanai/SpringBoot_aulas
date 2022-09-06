861379360d4e35c2158e97e267e6eaa2b1bb4768
cunhanai <dacunhanajulia@gmail.com>
2022-09-06 11:25:38 -0300

    detalhes do evento e cadastro de convidados por evento adicionados

--- src/main/java/com/entra21/eventoapp/controllers/EventoController.java
+++ src/main/java/com/entra21/eventoapp/controllers/EventoController.java
@@ -1,13 +1,19 @@
 package com.entra21.eventoapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.entra21.eventoapp.models.Convidado;
 import com.entra21.eventoapp.models.Evento;
+import com.entra21.eventoapp.repository.ConvidadoRepository;
 import com.entra21.eventoapp.repository.EventoRepository;
 
 @Controller
@@ -15,6 +21,9 @@ public class EventoController {
 
 	@Autowired
 	private EventoRepository er;
+
+	@Autowired
+	private ConvidadoRepository cr;
 
 	// PEGA O FORMULARIO A PARTIT DA REQUISICAO E MOSTRA NA WEB
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
@@ -39,13 +48,32 @@ public class EventoController {
 		mv.addObject("leventos", eventos); // leventos = atributo que esta no HTML
 		return mv;
 	}
-	
+
 	// CRIA UMA PAGINA DE DESCRICAO DO EVENTO
-	@RequestMapping("/{codigo}")
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
 		return mv;
+	}
+
+	// SALVA OS CONVIDADOS NO FORMULARIO
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado,
+			BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+	        attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+	        return "redirect:/{codigo}";
+	    }
+	        
+	    Evento evento = er.findByCodigo(codigo);
+	    convidado.setEvento(evento);
+	    cr.save(convidado);
+	    attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
+	    return "redirect:/{codigo}";
 	}
 }
